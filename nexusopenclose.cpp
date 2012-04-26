93850bf9706fe7393c65629789e77c011574168e
Peter Peterson <petersonpf@ornl.gov>
2012-04-26 10:25:06 -0400

    New test code for messing with loading a file. Only works on NOM_6190.

---
+++ nexusopenclose.cpp
@@ -0,0 +1,36 @@
+#include <iomanip>
+#include <iostream>
+#include <nexus/NeXusFile.hpp>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <sys/time.h>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+// -------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+    {
+      cout << "usage: " << argv[0] << " <filename>" << endl;
+      return 1;
+    }
+
+  string filename(argv[1]);
+  cout << "Trying with: " << filename << endl;
+  for (std::size_t i = 0; i < 1; i++)
+    {
+      NeXus::File file(filename);
+      file.openGroup("entry", "NXentry");
+      file.openGroup("bank47_events", "NXevent_data");
+      std::vector<uint32_t> data;//(11841710);
+      file.openData("event_id");
+      file.getData(data);
+      file.close();
+    }
+}
