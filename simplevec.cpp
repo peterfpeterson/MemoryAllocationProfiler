377b3afcb5c8f6edf22da61be23fe6781ce61250
Pete Peterson <petersonpf@ornl.gov>
2024-09-10 16:32:02 -0400

    Small code cleanup

--- simplevec.cpp
+++ simplevec.cpp
@@ -2,16 +2,14 @@
 #include <MantidKernel/CPUTimer.h>
 #include <MantidKernel/Memory.h>
 #include <MantidKernel/Timer.h>
-#include <ctime>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string>
-#include <sys/time.h>
 #include <unistd.h>
 #include <vector>
 
@@ -23,24 +21,16 @@ using std::endl;
 using std::string;
 
 int main(int argc, char *argv[]) {
-  int sleeptime = 2;
-  double rss, vm;
+  constexpr int sleeptime{2};
 
-  std::size_t i, n;
-  // char * buffer;
-
-  /*
-  cout << "How long do you want the string? ";
-  std::cin >> i;
-  */
-  i = 3000000000;
+  constexpr std::size_t VECTOR_LENGTH = 3000000000;
 
   CPUTimer timer;
   MemoryStats memory;
   // buffer = (char*) malloc (i);
   // if (buffer==NULL) exit (1);
-  std::vector<uint8_t> *buffer = new std::vector<uint8_t>();
-  buffer->resize(i);
+  auto *buffer = new std::vector<uint8_t>();
+  buffer->resize(VECTOR_LENGTH);
 
   memory.update();
   cout << "Allocate: " << timer << "\n" << memory << "\n";
@@ -48,8 +38,8 @@ int main(int argc, char *argv[]) {
   sleep(sleeptime);
   timer.reset();
 
-  for (n = 0; n < i; n++)
-    buffer->push_back(0); // static_cast<uint8_t>(rand()%256));
+  for (std::size_t n = 0; n < VECTOR_LENGTH; n++)
+    buffer->emplace_back(0); // static_cast<uint8_t>(rand()%256));
 
   memory.update();
   cout << "Fill: " << timer << "\n" << memory << "\n";
@@ -62,14 +52,8 @@ int main(int argc, char *argv[]) {
   delete buffer;
 
   memory.update();
-  cout << "Free: " << timer << "\n" << "FOU: " << memory << "\n";
+  cout << "Free: " << timer << "\n" << memory << "\n";
   timer.reset();
-
-  /*
-  for (n=0; n<i; n++)
-    static_cast<uint8_t>(rand()%256);
-  cout << "rand: " << timer << endl;
-  */
 
   return 0;
 }
